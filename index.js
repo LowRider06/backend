fe9229b0ab006c10d07196fd3affc2c86aa8bdb1
Sef <timseinacc@gmail.com>
2023-10-12 11:42:18 +0200

    changed mock data

--- index.js
+++ index.js
@@ -22,18 +22,8 @@ app.use(express.static('build'))
 let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+    name: "HTML is easy",
+    number: 1
   }
 ]
 
